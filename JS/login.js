a7b4f3bc705919e71978c4144428053bb20bfcba
karanjas39 <dhillonjaskaran4486@gmail.com>
2023-11-27 19:33:58 +0530

    login login

--- JS/login.js
+++ JS/login.js
@@ -16,8 +16,11 @@ let inputs = [
       </div>`,
 ];
 
+let username, password;
+
 // *****************************************************************************************************FUNCTIONS
 
+//! GENERAL FUNCTIONS
 // ONLOAD
 function details() {
   try {
@@ -53,7 +56,7 @@ function type() {
     document.querySelector(".terminal-last-login").textContent +=
       words[index].charAt(charIndex);
     charIndex++;
-    setTimeout(type, 100);
+    setTimeout(type, 50);
   } else {
     init_terminal();
   }
@@ -62,10 +65,62 @@ function type() {
 // INTI TERMINAL
 function init_terminal() {
   try {
+    count = 0;
     let container = document.querySelector(".terminal-inner");
     container.insertAdjacentHTML("beforeend", inputs[count]);
+    document
+      .querySelector(".terminal-inner")
+      .lastElementChild.querySelector("input")
+      .focus();
   } catch (error) {
     console.log(`Error: ${error.toString()} in init_terminalC`);
+  }
+}
+
+function loader(bool) {
+  try {
+    if (bool == 1) {
+      document.querySelector(".loader").classList.remove("hide");
+      document.querySelector(".blur").classList.remove("hide");
+    } else {
+      document.querySelector(".loader").classList.add("hide");
+      document.querySelector(".blur").classList.add("hide");
+    }
+  } catch (error) {
+    console.log(`Error: ${error.toString()} in loaderC`);
+  }
+}
+
+//! USER LOGIN
+async function userLogin() {
+  try {
+    loader(1);
+    let response = await fetch("http://127.0.0.1:4000/api/v1/admin/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    let data = await response.json();
+    loader(0);
+    let container = document.querySelector(".terminal-inner");
+    if (!!data && data.success == true) {
+      container.insertAdjacentHTML(
+        "beforeend",
+        "<p>[jas@portfolio ~]$ Login successful!</p>"
+      );
+      window.location.href = data.path;
+      sessionStorage.setItem("token", data.token);
+    } else {
+      container.insertAdjacentHTML(
+        "beforeend",
+        `<p>[jas@portfolio ~]$ Login fails : ${data.message}</p>`
+      );
+      init_terminal();
+    }
+  } catch (error) {
+    console.log(`Error: ${error.toString()} in userLoginC`);
   }
 }
 
@@ -101,30 +156,66 @@ document
   });
 
 //   TERMINAL INNER EVENTS
-document.querySelector(".terminal-inner").addEventListener("keydown", (e) => {
-  let value = document
-    .querySelectorAll(".terminal-inner div input")
-    [
-      document.querySelectorAll(".terminal-inner div input").length - 1
-    ].value.trim();
-  if (e.key == "Enter" && value != "") {
-    let container = document.querySelectorAll(".terminal-inner>div")[
-      document.querySelectorAll(".terminal-inner>div").length - 1
-    ];
-    let text =
-      count == 0
-        ? `<p>[jas@portfolio ~]$ Enter Username : ${
-            container.querySelector(".username").value
-          }</p>`
-        : `<p>[jas@portfolio ~]$ Enter Password : ${"*".repeat(
-            container.querySelector(".user-password").value.length
-          )}</p>`;
-    container.remove();
-    document
-      .querySelector(".terminal-inner")
-      .insertAdjacentHTML("beforeend", text);
-    count++;
-    if (count < 2) {
+document
+  .querySelector(".terminal-inner")
+  .addEventListener("keydown", async (e) => {
+    let value = document
+      .querySelectorAll(".terminal-inner div input")
+      [
+        document.querySelectorAll(".terminal-inner div input").length - 1
+      ].value.trim();
+    if (e.key == "Enter" && value != "") {
+      let container = document.querySelectorAll(".terminal-inner>div")[
+        document.querySelectorAll(".terminal-inner>div").length - 1
+      ];
+      if (count == 0) {
+        username = container.querySelector("input").value.trim();
+      }
+      if (count == 1) {
+        password = container.querySelector("input").value.trim();
+      }
+      let text =
+        count == 0
+          ? `<p>[jas@portfolio ~]$ Enter Username : ${
+              container.querySelector(".username").value
+            }</p>`
+          : `<p>[jas@portfolio ~]$ Enter Password : ${"*".repeat(
+              container.querySelector(".user-password").value.length
+            )}</p>`;
+      container.remove();
+      document
+        .querySelector(".terminal-inner")
+        .insertAdjacentHTML("beforeend", text);
+      count++;
+      if (count < 2) {
+        document
+          .querySelector(".terminal-inner")
+          .insertAdjacentHTML("beforeend", inputs[count]);
+        document
+          .querySelector(".terminal-inner")
+          .lastElementChild.querySelector("input")
+          .focus();
+      } else {
+        await userLogin();
+      }
+    }
+    if (e.key == "Enter" && value == "") {
+      let container = document.querySelectorAll(".terminal-inner>div")[
+        document.querySelectorAll(".terminal-inner>div").length - 1
+      ];
+      let text =
+        count == 0
+          ? `<p>[jas@portfolio ~]$ Enter Username : </p>`
+          : `<p>[jas@portfolio ~]$ Enter Password : </p>`;
+      container.remove();
+      document
+        .querySelector(".terminal-inner")
+        .insertAdjacentHTML("beforeend", text);
+      let notification =
+        count === 0 ? "Username cannot be empty" : "Password cannot be empty";
+      document
+        .querySelector(".terminal-inner")
+        .insertAdjacentHTML("beforeend", notification);
       document
         .querySelector(".terminal-inner")
         .insertAdjacentHTML("beforeend", inputs[count]);
@@ -132,40 +223,5 @@ document.querySelector(".terminal-inner").addEventListener("keydown", (e) => {
         .querySelector(".terminal-inner")
         .lastElementChild.querySelector("input")
         .focus();
-    } else {
-      document
-        .querySelector(".terminal-inner")
-        .lastElementChild.insertAdjacentHTML(
-          "beforeend",
-          `<div id="loading-animation"></div>`
-        );
-      document.getElementById("loading-animation").style.display = "block";
-      document.getElementById("loading-animation").classList.add("loading");
     }
-  }
-  if (e.key == "Enter" && value == "") {
-    let container = document.querySelectorAll(".terminal-inner>div")[
-      document.querySelectorAll(".terminal-inner>div").length - 1
-    ];
-    let text =
-      count == 0
-        ? `<p>[jas@portfolio ~]$ Enter Username : </p>`
-        : `<p>[jas@portfolio ~]$ Enter Password : </p>`;
-    container.remove();
-    document
-      .querySelector(".terminal-inner")
-      .insertAdjacentHTML("beforeend", text);
-    let notification =
-      count === 0 ? "Username cannot be empty" : "Password cannot be empty";
-    document
-      .querySelector(".terminal-inner")
-      .insertAdjacentHTML("beforeend", notification);
-    document
-      .querySelector(".terminal-inner")
-      .insertAdjacentHTML("beforeend", inputs[count]);
-    document
-      .querySelector(".terminal-inner")
-      .lastElementChild.querySelector("input")
-      .focus();
-  }
-});
+  });
